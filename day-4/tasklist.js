0c85ecca0ecae8c980b4584d0476d67a035a7f71
abubakr3800 <ahmedabubakr148@gmail.com>
2025-07-01 09:30:23 +0300

    tasklist

--- day-4/tasklist.js
+++ day-4/tasklist.js
@@ -1,7 +1,10 @@
 document.getElementById('addBtn').addEventListener('click', () => {
     const input = document.getElementById('taskInput');
-    const li = document.createElement('li');
-    li.textContent = input.value;
-    document.getElementById('taskList').appendChild(li);
+          list = document.getElementById('taskList');
+    // const li = document.createElement('li');
+    // li.textContent = input.value;
+    // document.getElementById('taskList').appendChild(li);
+
+    list.innerHTML += `<li>${input.value}</li>`;
     input.value = '';
-  });
+});
